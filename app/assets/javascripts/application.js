83b340cd97d06e94c66a995fad9adc9506ac7931
Fabricio Bonjorno <fabricio.bonjorno@gmail.com>
2020-02-29 15:01:59 -0400

    Config yarn and bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,9 @@
 // about supported directives.
 //
 //= require rails-ujs
+//= require jquery
+//= require bootstrap
+//= require popper
 //= require activestorage
 //= require turbolinks
 //= require_tree .
