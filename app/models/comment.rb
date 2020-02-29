fb05362eb380d5ec1c8ab003d86afb10fd0d348d
Fabricio Bonjorno <fabricio.bonjorno@gmail.com>
2020-02-29 15:23:32 -0400

    Config validates and references for models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,4 @@
 class Comment < ApplicationRecord
+  validates :author, :body, presence: true
   belongs_to :post
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,4 @@
 class Post < ApplicationRecord
+    validates :title, :author, :body, presence: true
+    has_many :comments
 end
