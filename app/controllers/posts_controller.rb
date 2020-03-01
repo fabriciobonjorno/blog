39599b60cc193b1f2b85312056a02be98b6f1158
Fabricio Bonjorno <fabricio.bonjorno@gmail.com>
2020-03-01 18:16:16 -0400

    Config search posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -35,9 +35,10 @@ end
 
     respond_to do |format|
       if @post.save
-        format.html { redirect_to @post, notice: 'Post was successfully created.' }
+        format.html { redirect_to @post, notice: 'Post criado com sucesso.' }
         format.json { render :show, status: :created, location: @post }
       else
+        flash.now[:alert] = 'Erro ao cadastrar o post'
         format.html { render :new }
         format.json { render json: @post.errors, status: :unprocessable_entity }
       end
@@ -49,7 +50,7 @@ end
   def update
     respond_to do |format|
       if @post.update(post_params)
-        format.html { redirect_to @post, notice: 'Post was successfully updated.'}
+        format.html { redirect_to @post, notice: 'Post atualizado com sucesso.'}
         format.json { render :show, status: :ok, location: @post }
       else
         format.html { render :edit }
